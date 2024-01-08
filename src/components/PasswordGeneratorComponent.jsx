276e39e938a29ba2a3cfd39cee7f193fe21fbf24
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2024-01-08 20:58:53 +0900

    Add files via upload

--- src/components/PasswordGeneratorComponent.jsx
+++ src/components/PasswordGeneratorComponent.jsx
@@ -1,20 +1,18 @@
-// PasswordGeneratorComponent.jsx
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Button } from 'antd';
 
 const PasswordGeneratorComponent = () => {
   const [passwordLength, setPasswordLength] = useState(8); // デフォルトのパスワード長さ
-  const [genetatePassword, setgeneratePassword] = useState('');
+  const [generatedPassword, setGeneratedPassword] = useState('');
 
   const handleGeneratePassword = () => {
     axios.post('/.netlify/functions/makePassword', { length: passwordLength })
       .then(response => {
-        const generatedPassword = response.data.password;
-        setgeneratePassword(generatedPassword);
+        const password = response.data.password;
+        setGeneratedPassword(password);
         // 生成されたパスワードを利用する処理をここに記述
-        console.log('Generated Password:', generatedPassword);
+        console.log('Generated Password:', password);
       })
       .catch(error => {
         console.error('Error generating password:', error);
@@ -30,7 +28,7 @@ const PasswordGeneratorComponent = () => {
         onChange={(e) => setPasswordLength(parseInt(e.target.value))}
       />
       <Button onClick={handleGeneratePassword}>Generate Password</Button>
-      
+      <p>{generatedPassword}</p>
     </div>
   );
 };
